988645c4dfadc94e35ec8f2004fa461b5f52d73e
JVThomas <jthomas0209@gmail.com>
2015-10-28 15:36:29 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  
+  def breed=(input_breed)
+    @breed = input_breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def name=(input_name)
+    @name = input_name
+  end
+
+  def name
+    @name
+  end
+end
